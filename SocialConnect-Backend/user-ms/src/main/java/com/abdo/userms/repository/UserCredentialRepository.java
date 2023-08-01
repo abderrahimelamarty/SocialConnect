434d36e97ce718b0eae1edbf9d27a6db3c092ba5
abderrahimelamarty <abdrahim.tawnat12345@gmail.com>
2023-08-01 01:16:42 +0100

    login+register

---
+++ SocialConnect-Backend/user-ms/src/main/java/com/abdo/userms/repository/UserCredentialRepository.java
@@ -0,0 +1,11 @@
+package com.abdo.userms.repository;
+
+
+import com.abdo.userms.entity.UserCredential;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+@Repository
+public interface UserCredentialRepository  extends JpaRepository<UserCredential,Integer> {
+    Optional<UserCredential> findByName(String username);
+}
