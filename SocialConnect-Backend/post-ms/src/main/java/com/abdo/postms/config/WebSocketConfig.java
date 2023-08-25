59b34d3ce98a3de360044821de25da89f436ab78
abderrahimelamarty <abdrahim.tawnat12345@gmail.com>
2023-08-25 23:36:55 +0100

    add private chat

--- SocialConnect-Backend/post-ms/src/main/java/com/abdo/postms/config/WebSocketConfig.java
+++ SocialConnect-Backend/post-ms/src/main/java/com/abdo/postms/config/WebSocketConfig.java
@@ -19,6 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws");
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws-message").setAllowedOriginPatterns("*").withSockJS();
+
     }
 }
